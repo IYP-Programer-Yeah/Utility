24aa57881d6fc7aeb21f9fa00e5564ee107e38fd
HoseinGhahremanzadeh <hgsilvermann@yahoo.com>
2019-02-21 17:39:35 +0330

    Add and improve tests.

---
+++ Projects/MSVC/2015/Test/integer-sequence-test.cpp
@@ -0,0 +1,145 @@
+#include <Utility/integer-sequence.inl>
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <vector>
+
+template <typename T, T... Is>
+std::vector<T> fill_vector(Utility::IntegerSequence<T, Is...>)
+{
+	return std::vector<T>{Is... };
+}
+
+TEST(IntegerSequenceTest, StepSizesOfOne)
+{
+	constexpr std::size_t Step = 1;
+	constexpr std::size_t Begin = 0;
+	constexpr std::size_t End = 100;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::size_t, End, Begin, Step>());
+
+	EXPECT_EQ(result[0], Begin);
+	EXPECT_LE((End - result.back())*Step, Step*Step);
+	auto expected_val = Begin;
+	for (auto current_val : result)
+	{
+		EXPECT_EQ(expected_val, current_val);
+		expected_val += Step;
+	}
+}
+
+TEST(IntegerSequenceTest, StepSizesOfN)
+{
+	constexpr std::size_t Step = 3;
+	constexpr std::size_t Begin = 0;
+	constexpr std::size_t End = 100;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::size_t, End, Begin, Step>());
+
+	EXPECT_EQ(result[0], Begin);
+	EXPECT_LE((End - result.back())*Step, Step*Step);
+	auto expected_val = Begin;
+	for (auto current_val : result)
+	{
+		EXPECT_EQ(expected_val, current_val);
+		expected_val += Step;
+	}
+}
+
+TEST(IntegerSequenceTest, NegetiveStep)
+{
+	constexpr std::int64_t Step = -1;
+	constexpr std::int64_t Begin = 100;
+	constexpr std::int64_t End = 0;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result[0], Begin);
+	EXPECT_LE((End - result.back())*Step, Step*Step);
+	auto expected_val = Begin;
+	for (auto current_val : result)
+	{
+		EXPECT_EQ(expected_val, current_val);
+		expected_val += Step;
+	}
+}
+
+TEST(IntegerSequenceTest, NegetiveStepAndEnd)
+{
+	constexpr std::int64_t Step = -3;
+	constexpr std::int64_t Begin = 100;
+	constexpr std::int64_t End = -10;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result[0], Begin);
+	EXPECT_LE((End - result.back())*Step, Step*Step);
+	auto expected_val = Begin;
+	for (auto current_val : result)
+	{
+		EXPECT_EQ(expected_val, current_val);
+		expected_val += Step;
+	}
+}
+
+TEST(IntegerSequenceTest, NegetiveStepBeginAndEnd)
+{
+	constexpr std::int64_t Step = -3;
+	constexpr std::int64_t Begin = -10;
+	constexpr std::int64_t End = -100;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result[0], Begin);
+	EXPECT_LE((End - result.back())*Step, Step*Step);
+	auto expected_val = Begin;
+	for (auto current_val : result)
+	{
+		EXPECT_EQ(expected_val, current_val);
+		expected_val += Step;
+	}
+}
+
+TEST(IntegerSequenceTest, EmptyNegetiveStepBeginAndEnd)
+{
+	constexpr std::int64_t Step = -3;
+	constexpr std::int64_t Begin = -100;
+	constexpr std::int64_t End = -10;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(IntegerSequenceTest, EmptyNegetiveBeginAndEnd)
+{
+	constexpr std::int64_t Step = 3;
+	constexpr std::int64_t Begin = -10;
+	constexpr std::int64_t End = -100;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(IntegerSequenceTest, EmptyNegetiveEnd)
+{
+	constexpr std::int64_t Step = 3;
+	constexpr std::int64_t Begin = 10;
+	constexpr std::int64_t End = -100;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result.size(), 0);
+}
+
+TEST(IntegerSequenceTest, Empty)
+{
+	constexpr std::int64_t Step = 3;
+	constexpr std::int64_t Begin = 100;
+	constexpr std::int64_t End = 10;
+
+	auto result = fill_vector(Utility::MakeIntegerSequence<std::int64_t, End, Begin, Step>());
+
+	EXPECT_EQ(result.size(), 0);
+}
--- Projects/MSVC/2015/Test/tuple-test.cpp
+++ Projects/MSVC/2015/Test/tuple-test.cpp
@@ -8,27 +8,27 @@ static std::size_t count = 0;
 template <std::size_t N>
 struct IntegerType {};
 
-void PassAsTupleTestFunction1(IntegerType<0>) { count = 1; }
-void PassAsTupleTestFunction2(IntegerType<0>, IntegerType<1>) { count = 2; }
-void PassAsTupleTestFunction3(IntegerType<0>, IntegerType<1>, IntegerType<2>) { count = 3; }
-void PassAsTupleTestFunction4(IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>) { count = 4; }
-void PassAsTupleTestFunction5(IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>, IntegerType<4>) { count = 5; }
+void PassTupleElementsTestFunction1(IntegerType<0>) { count = 1; }
+void PassTupleElementsTestFunction2(IntegerType<0>, IntegerType<1>) { count = 2; }
+void PassTupleElementsTestFunction3(IntegerType<0>, IntegerType<1>, IntegerType<2>) { count = 3; }
+void PassTupleElementsTestFunction4(IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>) { count = 4; }
+void PassTupleElementsTestFunction5(IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>, IntegerType<4>) { count = 5; }
 
-TEST(PassAsTupleTestFunction, Simple)
+TEST(PassTupleElementsTest, Simple)
 {
 	count = 0;
-	Utility::call_with_tuple_elements(PassAsTupleTestFunction1, std::tuple<IntegerType<0>>());
+	Utility::call_with_tuple_elements(PassTupleElementsTestFunction1, std::tuple<IntegerType<0>>());
 	EXPECT_EQ(count, 1);
 	count = 0;
-	Utility::call_with_tuple_elements(PassAsTupleTestFunction2, std::tuple<IntegerType<0>, IntegerType<1>>());
+	Utility::call_with_tuple_elements(PassTupleElementsTestFunction2, std::tuple<IntegerType<0>, IntegerType<1>>());
 	EXPECT_EQ(count, 2);
 	count = 0;
-	Utility::call_with_tuple_elements(PassAsTupleTestFunction3, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>>());
+	Utility::call_with_tuple_elements(PassTupleElementsTestFunction3, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>>());
 	EXPECT_EQ(count, 3);
 	count = 0;
-	Utility::call_with_tuple_elements(PassAsTupleTestFunction4, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>>());
+	Utility::call_with_tuple_elements(PassTupleElementsTestFunction4, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>>());
 	EXPECT_EQ(count, 4);
 	count = 0;
-	Utility::call_with_tuple_elements(PassAsTupleTestFunction5, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>, IntegerType<4>>());
+	Utility::call_with_tuple_elements(PassTupleElementsTestFunction5, std::tuple<IntegerType<0>, IntegerType<1>, IntegerType<2>, IntegerType<3>, IntegerType<4>>());
 	EXPECT_EQ(count, 5);
 }
